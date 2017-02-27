6a148b21fd089ea90def59c0c6ccd90aba88e085
David Ebbo <david.ebbo@gmail.com>
2017-02-26 21:32:43 -0800

    Send http reqs and wait for them

--- HttpReq/Program.cs
+++ HttpReq/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HttpReq
 {
@@ -6,7 +9,37 @@ namespace HttpReq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DoWorkAsync(args[0], Int32.Parse(args[1]), Int32.Parse(args[2])).Wait();
+        }
+
+        static async Task DoWorkAsync(string url, int iterations, int delay)
+        {
+            var tasks = new List<Task>();
+
+            using (var client = new HttpClient())
+            {
+
+                for (int i = 0; i < iterations; i++)
+                {
+                    Console.WriteLine($"Starting request {i}");
+                    tasks.Add(client.GetStringAsync(url).ContinueWith(RequestDone, new InvocationState { Start = DateTime.Now, Iteration = i }));
+                    await Task.Delay(delay);
+                }
+
+                await Task.WhenAll(tasks);
+            }
+        }
+
+        static void RequestDone(Task<string> action, object state)
+        {
+            var invocationState = (InvocationState)state;
+            Console.WriteLine($"{invocationState.Iteration}: {DateTime.Now - invocationState.Start}: {action.Result}");
+        }
+
+        class InvocationState
+        {
+            public DateTime Start { get; set; }
+            public int Iteration { get; set; }
         }
     }
 }
